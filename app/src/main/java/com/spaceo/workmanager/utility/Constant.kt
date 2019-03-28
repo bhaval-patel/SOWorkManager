49ba8500eba31e48338b119609c0026d38edf525
AmitP <amit_patoliya@spaceotechnologies.com>
2019-03-28 10:21:37 +0530

    license added

--- app/src/main/java/com/spaceo/workmanager/utility/Constant.kt
+++ app/src/main/java/com/spaceo/workmanager/utility/Constant.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2010 - 2019 Space-O Technologies.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.spaceo.workmanager.utility
 
 const val PERMISION_REQUEST = 100
